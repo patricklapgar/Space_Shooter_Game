76a004eae3cf75dd0bf195f4c24121f90376a59b
patricklapgar <patricklapgardeveloper@gmail.com>
2020-08-08 02:15:30 -0500

    Created window with background that displays the current level and the number of lives left.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 # Initialize pygame window
 WIDTH, HEIGHT = 750, 750
@@ -24,17 +25,32 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Upload background image
-BACKGROUND = pygame.image.load(os.path.join("assets", "background-black.png"))
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 # Create main event loop to handle all events
 def main():
     run = True
     # Frame rate
     FPS = 60
+    level = 1
+    lives = 5
     clock = pygame.time.Clock()
+    main_font = pygame.font.SysFont("comicsans", 50)
+
+    def redraw_window():
+        # The blit method takes one of the images provided and draws it on the window
+        # Draw image background at point 0, 0
+        WINDOW.blit(BACKGROUND, (0,0))      
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+        WINDOW.blit(lives_label, (10, 10))
+        WINDOW.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        pygame.display.update()
 
     while run:
         clock.tick(FPS)
+        redraw_window()
         for event in pygame.event.get():
             # if event has occured, do something
             if event.type == pygame.QUIT:
